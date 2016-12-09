c9beff32c2860d8700c3fce51646c17dde3fa021
Arjhun Srinivas <arjhun.s@gmail.com>
2016-12-10 03:00:13 +0530

    Bugg

--- server.js
+++ server.js
@@ -56,6 +56,8 @@ app.use(session({
   ttl: 9999
 }));
 
+app.options('*', cors());
+
 app.use(function(req, res, next) {
   // var oneof = false;
   // res.header('Access-Control-Allow-Credentials', true);
@@ -76,7 +78,7 @@ app.use(function(req, res, next) {
   // }
 
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', 'http://casanyla.com');
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
 
