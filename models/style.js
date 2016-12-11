35f4be85d814421abc3029a01ffbf0691d0bf950
Arjhun Srinivas <arjhun.s@gmail.com>
2016-12-11 18:49:10 +0530

    Added style model attribute

--- models/style.js
+++ models/style.js
@@ -18,6 +18,11 @@ var styleSchema = new mongoose.Schema({
       trim: true,
       required: true
     },
+    path: {
+      type: String,
+      trim: true,
+      required: true
+    },
     active: {
       type: Boolean,
       required: true,
