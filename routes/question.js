a1c461140d97bba9cd4d2585d130974a986e3abd
Arjhun Srinivas <arjhun.s@gmail.com>
2016-12-12 02:36:19 +0530

    Added questions functionalities

---
+++ routes/question.js
@@ -0,0 +1,83 @@
+var express = require('express');
+
+var router = express.Router();
+var Question = require('../models/question');
+
+/*********************
+ * User level routes *
+ ********************/
+router.get('/', function(req, res) {
+  // Returns all questions
+  // PARAMS : No params
+  Question.find({}, function(err, docs) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(docs);
+    }
+  });
+  // Question.find({})
+  //   .populate('options.relatedStyles', '_id name')
+  //   .exec(function(err, docs) {
+  //     if (err) {
+  //       res.send(err)
+  //     } else {
+  //       res.send(docs);
+  //     }
+  //   });
+});
+
+/*********************
+ * Admin level routes *
+ *********************/
+router.use(function(req, res, next) {
+  // Allow only admins to access following endpoints
+  if (req.session.role == 'admin') {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+router.post('/', function(req, res) {
+  // Creates a new question in DB
+  // PARAMS : Refer associated model
+  var newQuestion = new Question(req.body);
+  newQuestion.save(function(err, doc) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(doc);
+    }
+  });
+});
+
+router.put('/:questionId', function(req, res) {
+  // Updates existing style in DB
+  // PARAMS : MongoID as part of URL
+  // PARAMS : Update object with new data
+  Question.findOneAndUpdate({
+    _id: req.params.questionId
+  }, req.body, {
+    upsert: false
+  }, function(err, doc) {
+    if (err) throw err;
+    res.send(doc);
+  });
+});
+
+router.delete('/:questionId', function(req, res) {
+  // Deletes existing style in DB
+  // PARAMS : MongoID as part of URL
+  Question.find({
+    _id: req.params.questionId
+  }).remove().exec(function(err, removed) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(removed);
+    }
+  });
+});
+
+module.exports = router;
