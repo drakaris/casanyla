185c3cd91af20fb1341d3697abd5b9eb94bf3cd6
Arjhun Srinivas <arjhun.s@gmail.com>
2016-12-11 21:21:20 +0530

    Updated question model

---
+++ models/question.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var questionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  options: [{
+    name: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    image: {
+      type: String,
+      trim: true,
+      required: true,
+      default: 'default.png'
+    },
+    relatedStyles: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Style'
+    }]
+  }],
+  active: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+});
+
+projectSchema.methods.toJSON = function() {
+  var obj = this.toObject();
+  delete obj.__v;
+  delete obj.createdAt;
+  return obj;
+};
+
+var Question = mongoose.model('Question', questionSchema);
+module.exports = Question;
